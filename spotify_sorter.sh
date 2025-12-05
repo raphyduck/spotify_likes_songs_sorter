171c43f6a8af4478aa1f8ef76a3e8f226a16b392
raphyduck <raphy@hobbitton.at>
2025-12-05 16:25:02 +0100

    bugfix

--- spotify_sorter.sh
+++ spotify_sorter.sh
@@ -2,10 +2,10 @@
 # shellcheck shell=bash
 
 
-if [ ! -f ~/.venv/bin/activate ]; then
+if [ ! -f ~/venv/bin/activate ]; then
   echo "Expected virtual environment at .venv. Please run 'python -m venv .venv' and install dependencies." >&2
   exit 1
 fi
 
-source ~/.venv/bin/activate
+source ~/venv/bin/activate
 python ./spotify_sorter.py
